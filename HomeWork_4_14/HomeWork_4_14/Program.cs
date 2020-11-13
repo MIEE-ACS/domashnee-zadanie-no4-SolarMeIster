c1abbf1a3c30ea8a1b74879b2f40c4e8ae4fbfbf
SolarMeIster <71086378+SolarMeIster@users.noreply.github.com>
2020-11-13 14:48:22 +0300

    Correct HomeWork_4

--- HomeWork_4_14/HomeWork_4_14/Program.cs
+++ HomeWork_4_14/HomeWork_4_14/Program.cs
@@ -8,6 +8,7 @@ namespace HomeWork_4_14
         {
             Console.WriteLine("Для использования одномерного массива.....1");
             Console.WriteLine("Для использования двумерного массива.....2");
+            Console.WriteLine("Выход из программы.....0");
             Console.Write("Выберите из выше перечисленного: ");
             number = int.Parse(Console.ReadLine());
             return number;
@@ -15,23 +16,27 @@ namespace HomeWork_4_14
         static void Main(string[] args)
         {
             int number = 0;
-            number = InterFace(number);
-            int k = 0;
-            switch (number)
+            while (true)
             {
-                case 1:
+                Console.Clear();
+                number = InterFace(number);
+                int k = 0;
+                if (number == 1)
+                {
+                    Console.Clear();
+                    Console.Write("Введите количество элементов массива: ");
+                    int n = int.Parse(Console.ReadLine());
+                    if (n > 0)
                     {
-                        Console.Clear();
-                        Console.Write("Введите количество элементов массива: ");
-                        int n = int.Parse(Console.ReadLine());
                         double[] mass = new double[n];
                         Random rand = new Random();
                         for (int i = 0; i < n; i++)
                         {
                             double rnd = rand.NextDouble() * 5;
                             mass[i] += rnd;
-                            Console.Write("{0:0.00} ", mass[i]);
-                            if (mass[i] == 0) {
+                            Console.Write("{0:0.0} ", mass[i]);
+                            if (mass[i] == 0)
+                            {
                                 k++;
                             }
                         }
@@ -58,50 +63,83 @@ namespace HomeWork_4_14
                         Console.Write("Отсортированный массив: ");
                         for (int i = 0; i < n; i++)
                         {
-                            Console.Write("{0:0.00} ", mass[i]);
+                            Console.Write("{0:0.0} ", mass[i]);
                         }
                         Console.WriteLine();
-                        Console.WriteLine("Сумма элементов массива, расположенных после минимального элемента: {0:0.00}", sum);
+                        Console.WriteLine("Сумма элементов массива, расположенных после минимального элемента: {0:0.0}", sum);
                         Console.WriteLine($"Количество нулей в массиве: {k}");
                         break;
                     }
-                case 2:
+                    else
                     {
-                        Console.Clear();
-                        Console.Write("Введите количество строк и столбцов: ");
-                        int n = int.Parse(Console.ReadLine());
-                        Random rand = new Random();
-                        int[,] dvumern_mass = new int[n, n];
-                        int[,] dop_mass = new int[n, n];
-                        for (int i = 0; i < n; i++)
+                        Console.WriteLine("Ошибка ввода");
+                        Console.ReadKey();
+                    }
+                }
+                if (number == 2)
+                {
+                    Console.Clear();
+                    int j, l, num, num2, num3, p;
+                    Console.Write("Введите количество строк и столбцов: ");
+                    int n = int.Parse(Console.ReadLine());
+                    Console.Write("Введите кол-во шагов: ");
+                    p = int.Parse(Console.ReadLine());
+                    Random rand = new Random();
+                    int[,] dvumern_mass = new int[n, n];
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (j = 0; j < n; j++)
                         {
-                            for (int j = 0; j < n; j++)
-                            {
-                                int rnd_2 = rand.Next(10);
-                                dvumern_mass[i, j] += rnd_2;
-                                Console.Write($"{dvumern_mass[i, j]} ");
-                            }
-                            Console.WriteLine();
+                            int rnd_2 = rand.Next(10);
+                            dvumern_mass[i, j] += rnd_2;
+                            Console.Write($"{dvumern_mass[i, j]} ");
                         }
                         Console.WriteLine();
-                        for (int i = 0; i < n; i++)
-                        {
-                            for (int j = 0; j < n; j++)
-                            {
-                                dop_mass[n - 1 - i, n - 1 - j] = dvumern_mass[i, n - 1 - j];
-                            }
-                        }
-                        Console.WriteLine("Сдвинутая матриица:");
-                        for (int i = 0; i < n; i++)
-                        {
-                            for (int j = 0; j < n; j++)
-                            {
-                                Console.Write($"{dop_mass[j, i]} ");
-                            }
-                            Console.WriteLine();
-                        }
-                        break;
                     }
+                    Console.WriteLine();
+                    for (int i = 0; i < n / 2; i++)
+                    {
+                        for (int z = p; z > 0; z--)
+                        {
+                            for (j = i, num = dvumern_mass[i, j]; j < n - 1 - i; j++)
+                            {
+                                num2 = dvumern_mass[i, j + 1];
+                                dvumern_mass[i, j + 1] = num;
+                                num = num2;
+                            }
+                            for (l = i + 1; l < n - i; l++)
+                            {
+                                num3 = num;
+                                num = dvumern_mass[l, j];
+                                dvumern_mass[l, j] = num3;
+                            }
+                            for (j--, l--; j >= i; j--)
+                            {
+                                num3 = num;
+                                num = dvumern_mass[l, j];
+                                dvumern_mass[l, j] = num3;
+                            }
+
+                            for (j++, l--; l >= i; l--)
+                            {
+                                num3 = num;
+                                num = dvumern_mass[l, j];
+                                dvumern_mass[l, j] = num3;
+                            }
+                        }
+                    }
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (j = 0; j < n; j++)
+                            Console.Write($"{dvumern_mass[i, j]} ");
+                        Console.WriteLine();
+                    }
+                break;
+                }
+                if (number == 0)
+                {
+                    break;
+                }
             }
         }
     }
